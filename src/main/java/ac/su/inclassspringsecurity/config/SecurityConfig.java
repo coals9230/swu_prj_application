fa0854a2c85ce521fe1cc5d2e61ad452af226593
coals9230 <98140128+coals9230@users.noreply.github.com>
2024-07-24 10:22:10 +0900

    Update SecurityConfig.java

--- src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
+++ src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
@@ -18,53 +18,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final TokenProvider tokenProvider;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorizeHttpRequests -> 
-                authorizeHttpRequests.requestMatchers(
-                    new AntPathRequestMatcher("/**")
-                ).permitAll()
-            )
-            .csrf().disable()  // CSRF 보호 비활성화
-            .headers(headers -> 
-                headers.addHeaderWriter(
-                    new XFrameOptionsHeaderWriter(
-                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
-                    )
-                )
-            )
-            .formLogin(formLogin -> 
-                formLogin
-                    .loginPage("/users/login")
-                    .defaultSuccessUrl("/")
-            )
-            .logout(logout -> 
-                logout
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
-                    .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true)
-            )
-//            .sessionManagement(sessionConfig -> 
-//                sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            )
-//            .addFilterBefore(
-//                tokenAuthenticationFilter(),
-//                UsernamePasswordAuthenticationFilter.class
-//            )
-        ;
+            .csrf().disable() // CSRF 보호 비활성화
+            .authorizeRequests()
+            .anyRequest().permitAll();
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-        return new TokenAuthenticationFilter(tokenProvider);
     }
 }
