4dd16f0d07920beaceaced66aaf6b36c97fe6d42
coals9230 <98140128+coals9230@users.noreply.github.com>
2024-07-24 11:17:27 +0900

    Update SecurityConfig.java

--- src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
+++ src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
@@ -2,28 +2,80 @@ package ac.su.inclassspringsecurity.config;
 
 import ac.su.inclassspringsecurity.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  // URL 요청에 대한 Spring Security 동작 활성화
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement((session) -> 
-                              session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.csrf(AbstractHttpConfigurer::disable);
+        http
+            .csrf().disable()
+            .authorizeHttpRequests(  // 요청 인가 여부 결정을 위한 조건 판단
+                (authorizeHttpRequests) ->
+                    authorizeHttpRequests.requestMatchers(
+                        new AntPathRequestMatcher("/**")
+                    ).permitAll()
+
+            .csrf(
+                (csrf) -> csrf.ignoringRequestMatchers(
+                    new AntPathRequestMatcher("/api/**")
+                    , new AntPathRequestMatcher("/users/login")
+                )
+            )
+            .headers(
+                (headers) ->
+                    headers.addHeaderWriter(
+                        new XFrameOptionsHeaderWriter(
+                            // X-Frame-Options 는 웹 페이지 내에서 다른 웹 페이지 표시 허용 여부 제어
+                            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN  // 동일 도메인 내에서 표시 허용
+                        )
+                    )
+            )
+            .formLogin(
+                (formLogin) ->
+                    formLogin  // Controller 에 PostMapping URL 바인딩이 없어도
+                               // POST 요청을 아래 라인에서 수신하고 인증 처리
+                        .loginPage("/users/login")
+                        .defaultSuccessUrl("/")
+            )
+            .logout(
+                (logout) ->
+                    logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+            )
+
+        ;
         return http.build();
+    }
+
+    // passwordEncoder 빈 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
     }
 }
